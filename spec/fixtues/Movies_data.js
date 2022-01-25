8ee70d38edea61c97f49835f542bf3dd0fc3235c
Mirunalini Ravichandran <rmirunalini@zeomega.com>
2022-01-25 12:09:40 +0530

    movie

---
+++ spec/fixtues/Movies_data.js
@@ -0,0 +1,32 @@
+
+
+//restorents information are stored in this file using Fixtures
+var FIXTURES = window.FIXTURES || {};
+
+FIXTURES.movies ={
+
+    movie1:{
+        name:"Anjaan",
+        rating:"4.5",
+        genres:"comedy",
+        comment:"very Good"
+    },
+    movie2:{
+        name:"Kathi",
+        rating:"4.8",
+        genres:"adventure",
+        comment:"excelent movie"
+    },
+    movie3:{
+        name:"Bigil",
+        rating:"4.3",
+        genres:"action",
+        comment:"Good"
+    },
+    movie4:{
+        name:"Beast",
+        rating:"4.5",
+        genres:"horror",
+        comment:"Good"
+    },
+}
